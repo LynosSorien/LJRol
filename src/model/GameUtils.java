72d40cd0a69768e0e86279c2f402ab2beda86b28
xinsen <juanma_1267@hotmail.com>
2014-09-08 10:21:30 +0200

    ready to merge

--- src/model/GameUtils.java
+++ src/model/GameUtils.java
@@ -27,25 +27,24 @@ public final class GameUtils {
         int row = 0;
         while ((line = br.readLine().toUpperCase()) != null) {
             for (int col = 0; col < line.length(); col++) {
-                if (type.equals("TERRAIN"))
-                    if (line.charAt(col)=='G' || line.charAt(col)=='T' || line.charAt(col)=='S'){
-                        map.setElement(row,col,new WalkableTerrain(""+line.charAt(col)));
-                    }else if (line.charAt(col) == 'W'  || line.charAt(col)=='A' || line.charAt(col)=='O'){
-                        map.setElement(row,col,new UnwalkableTerrain(""+line.charAt(col)));
-                    }else   map.setElement(row,col,new WalkableTerrain("G"));
-                else if (type.equals("OBJECT"))
-                    if (line.charAt(col)=='L')//life potion
-                        map.setElement(row,col,new UsableItem(UsableItem.ItemProperty.HEALTHY,20));
-                    else if (line.charAt(col)=='W')//weapon
-                        map.setElement(row,col,new Weapon(60));
-                    else if (line.charAt(col)=='P')//player
-                        map.setElement(row,col,model.getPlayer());
-                    else if (line.charAt(col)=='N')//npc
-                        map.setElement(row,col,new Npc("NPC",100));
+                if (type.equals("TERRAIN")) {
+                    if (line.charAt(col) == 'G' || line.charAt(col) == 'T' || line.charAt(col) == 'S') {
+                        map.setElement(row, col, new WalkableTerrain("" + line.charAt(col)));
+                    } else if (line.charAt(col) == 'W' || line.charAt(col) == 'A' || line.charAt(col) == 'O') {
+                        map.setElement(row, col, new UnwalkableTerrain("" + line.charAt(col)));
+                    } else map.setElement(row, col, new WalkableTerrain("G"));
+                }else if (type.equals("OBJECT")) {
+                    if (line.charAt(col) == 'L')//life potion
+                        map.setElement(row, col, new UsableItem(UsableItem.ItemProperty.HEALTHY, 20));
+                    else if (line.charAt(col) == 'W')//weapon
+                        map.setElement(row, col, new Weapon(60));
+                    else if (line.charAt(col) == 'P')//player
+                        map.setElement(row, col, model.getPlayer());
+                    else if (line.charAt(col) == 'N')//npc
+                        map.setElement(row, col, new Npc("NPC", 100));
+                }
             }
             row++;
         }
     }
-
-
 }
